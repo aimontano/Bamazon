8df6c279e2d73ed29ccce6bb51d8ee0d432f7362
aimontano <aimorales.montano@gmail.com>
2018-10-23 20:12:39 -0400

    Added quantity update from bamazon file

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,6 +1,5 @@
 const inquirer = require('inquirer');
 const bamazon = require('./bamazon.js');
-const cTable = require('console.table');
 
 // ask user for product they would like to buy + quantity
 const getUserInput = () => {
@@ -25,23 +24,6 @@ const getUserInput = () => {
 	});	
 }
 
-// Updates database base on product and quantity bought
-const updateProductQuantity = (id, quantity) => {
-	bamazon.db.query(
-		"UPDATE products SET ? WHERE ?",
-		[
-			{
-				stock_quantity: quantity
-			}, 
-			{
-				id: id
-			}
-		],
-		(err, res) => {
-			if(err) throw err;
-		})
-}
-
 // Checks if you can buy the product
 const buyProduct = (id, quantity) => {
 	bamazon.db.query(
@@ -52,42 +34,29 @@ const buyProduct = (id, quantity) => {
 		(err, res) => {
 			if(err) throw err;
 
-			let itemsAvailable = res[0].stock_quantity; // stores available items
+			if(res[0] == undefined) {
+				console.log("Item does not exists");
+				getUserInput();
+			} else {
+				let itemsAvailable = res[0].stock_quantity; // stores available items
 
-			if(itemsAvailable < quantity){
-				console.log("Insufficient Quantity!!");
+				if(itemsAvailable < quantity){
+					console.log("Insufficient Quantity!!");
+				}
+				else {
+					let updatedQuantity = itemsAvailable - quantity;
+
+					bamazon.updateQuantity(id, updatedQuantity);
+					console.log(`Your're total is ${(res[0].price * quantity).toFixed(2)}`);
+					bamazon.db.end();
+				}
 			}
-			else {
-				let updatedQuantity = itemsAvailable - quantity;
-				updateProductQuantity(id, updatedQuantity);
-				console.log(`Your're total is ${(res[0].price * quantity).toFixed(2)}`);
-			}
-			bamazon.db.end();
 		});
 }
 
 // functions logs each item on the products table
 const displayProducts = () => {
-	bamazon.db.query("SELECT * FROM products", (err, res) => {
-		if(err) throw err;
-	
-		let tableItems = [];	
-
-		// push each product as an object to tableItems 
-		res.forEach(item => {
-			tableItems.push({
-				id: item.id,
-				name: item.product_name,
-				department: item.department_name,
-				price: item.price,
-				quantity: item.stock_quantity
-			});
-		})
-		// logs each item as a table
-		console.table(tableItems);
-
-		getUserInput();
-	});
+	bamazon.printData(null, getUserInput);
 }
 
 // connect to database
@@ -95,5 +64,4 @@ bamazon.db.connect(err => {
 	if (err) throw err;
 	// if connected display all the products
 	displayProducts();
-})
-
+});
